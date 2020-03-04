1d61e1405e498315ff3d37d47cec1548084fc94a
Tabulate <58576759+TabulateJarl8@users.noreply.github.com>
2020-03-04 17:20:17 -0500

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="randfacts",
+    version="0.0.2",
+    author="Tabulate",
+    author_email="tabulatejarl8@gmail.com",
+    description="Package to generate random facts",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/TabulateJarl8/randfacts",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
