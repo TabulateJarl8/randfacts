3a41a2dd29f1102ba07afddeb57bcd6aaa7b7788
Tabulate <tabulatejarl8@gmail.com>
2020-05-18 23:33:38 -0400

    Add files via upload

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="randfacts",
-    version="0.1.8",
+    version="0.1.10",
     author="Tabulate",
     author_email="tabulatejarl8@gmail.com",
     description="Package to generate random facts",
