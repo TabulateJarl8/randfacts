fad6eb4ab3cdfdc9f7fc23a0ba64cd69edecb52c
TabulateJarl8 <tabulatejarl8@gmail.com>
2020-06-23 14:30:04 -0400

    fixy

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="randfacts",
-    version="0.1.10",
+    version="0.1.11",
     author="Tabulate",
     author_email="tabulatejarl8@gmail.com",
     description="Package to generate random facts",
