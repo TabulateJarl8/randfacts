43c0569be192fe4759aa818af2ad320c2449392f
TabulateJarl8 <tabulatejarl8@gmail.com>
2020-08-21 21:49:45 -0400

    remove bad import

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="randfacts",
-    version="0.2.2",
+    version="0.2.3",
     author="Tabulate",
     author_email="tabulatejarl8@gmail.com",
     description="Package to generate random facts",
