445f9a5346f8f9a414837cc535a650de8697e120
TabulateJarl8 <tabulatejarl8@gmail.com>
2021-02-26 11:24:57 -0500

    make checkduplicates test better and fix main including blank strings

--- randfacts/main.py
+++ randfacts/main.py
@@ -4,9 +4,9 @@ import os
 def getFact(filter=True):
 	dir_path = os.path.dirname(os.path.realpath(__file__))
 	with open(os.path.join(dir_path, "safe.txt")) as f:
-		safelist = [fact.rstrip('\r\n ') for fact in f.readlines()]
+		safelist = [fact.rstrip('\r\n ') for fact in f.readlines() if fact != '']
 	with open(os.path.join(dir_path, "unsafe.txt")) as f:
-		unsafelist = [fact.rstrip('\r\n ') for fact in f.readlines()]
+		unsafelist = [fact.rstrip('\r\n ') for fact in f.readlines() if fact != '']
 	if filter == False:
 		safelist += unsafelist
 	return safelist[randint(0, len(safelist) - 1)]
--- tests/checkduplicates.py
+++ tests/checkduplicates.py
@@ -1,6 +1,7 @@
 from fuzzywuzzy import fuzz
 import os.path
 import itertools
+from tqdm import tqdm
 
 parent = os.path.abspath(os.path.dirname(os.path.dirname(__file__)))
 with open(os.path.join(parent, 'randfacts', 'safe.txt')) as f:
@@ -22,7 +23,17 @@ def partial_match(x, y, xindex, yindex):
 combinations = itertools.combinations(enumerate(safe + unsafe), 2)
 combinations = [[[idx1, idx2], [arr1, arr2]] for ((idx1, arr1), (idx2, arr2)) in combinations]
 
-matches = [partial_match(item[1][0], item[1][1], item[0][0], item[0][1]) for item in combinations if partial_match(item[1][0], item[1][1], item[0][0], item[0][1]) != None]
+matches = []
+print()
+with tqdm(total=len(combinations)) as pbar:
+	for item in combinations:
+		match = partial_match(item[1][0], item[1][1], item[0][0], item[0][1])
+		if match != None:
+			matches.append(match)
+		pbar.update(1)
+print()
 if matches != []:
 	print(matches)
 	exit(2)
+else:
+	exit()
