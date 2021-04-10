4e43a517dbc30dd1337ca04e2f285dd6d076ca72
TabulateJarl8 <tabulatejarl8@gmail.com>
2021-04-10 15:43:48 -0400

    fix again

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ setuptools.setup(
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
-        "Natural Language :: English"
+        "Natural Language :: English",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
