5f4aa51b836c9e13b6056a6b399395bf98aaa909
TabulateJarl8 <tabulatejarl8@gmail.com>
2021-07-10 15:44:18 -0400

    more facts

--- tests/checkduplicates.py
+++ tests/checkduplicates.py
@@ -1,26 +1,30 @@
-from fuzzywuzzy import fuzz
+from rapidfuzz import fuzz
 import os.path
 import itertools
 from tqdm import tqdm
+import argparse
 
 parent = os.path.abspath(os.path.dirname(os.path.dirname(__file__)))
 with open(os.path.join(parent, 'randfacts', 'safe.txt')) as f:
-	safe = [line.rstrip() for line in f.readlines()]
+	safe = [(line.rstrip(), 'safe') for line in f.readlines()]
 
 with open(os.path.join(parent, 'randfacts', 'unsafe.txt')) as f:
-	unsafe = [line.rstrip() for line in f.readlines()]
+	unsafe = [(line.rstrip(), 'unsafe') for line in f.readlines()]
 
 
 def partial_match(x, y, xindex, yindex):
 	if xindex == yindex:
 		return None
 	else:
-		ratio = fuzz.token_sort_ratio(x, y)
+		ratio = fuzz.token_sort_ratio(x[0], y[0])
 		if ratio > 80:
 			return (x, y), (xindex, yindex), ratio
 		else:
 			return None
 
+parser = argparse.ArgumentParser()
+parser.add_argument('--fix-duplicates', action='store_true', help='Remove duplicate facts')
+args = parser.parse_args()
 
 print('Generating combinations...')
 combinations = itertools.combinations(enumerate(safe + unsafe), 2)
@@ -35,10 +39,37 @@ with tqdm(total=len(combinations)) as pbar:
 			matches.append(match)
 		pbar.update(1)
 print()
+
 if matches != []:
-	print('\n'.join([str(match) for match in matches]))
-	print()
-	print('Number of similar facts: ' + str(len(matches)))
-	exit(2)
+	if not args.fix_duplicates:
+		print('\n'.join([str(match) for match in matches]))
+		print()
+		print('Number of similar facts: ' + str(len(matches)))
+		exit(2)
+	else:
+		# iterate through matches and remove duplicates from combinations
+		print('Generating list of indexes to remove...')
+		indexes_to_remove = []
+		for match in matches:
+			# keep unsafe facts over safe facts
+			if match[0][0][1] == 'unsafe':
+				indexes_to_remove.append(match[1][1])
+			elif match[0][1][1] == 'unsafe':
+				indexes_to_remove.append(match[1][0])
+			else:
+				indexes_to_remove.append(match[1][0])
+
+		print('Removing duplicates from facts...')
+		facts = safe + unsafe
+		for index in sorted(indexes_to_remove, reverse=True):
+			del facts[index]
+
+		safe = [fact for fact, correct_list in facts if correct_list == 'safe']
+		unsafe = [fact for fact, correct_list in facts if correct_list == 'unsafe']
+
+		with open(os.path.join(parent, 'randfacts', 'safe.txt'), 'w') as f:
+			f.write('\n'.join(safe))
+		with open(os.path.join(parent, 'randfacts', 'unsafe.txt'), 'w') as f:
+			f.write('\n'.join(unsafe))
 else:
 	exit()
