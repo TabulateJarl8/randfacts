95c2b79f07c192ffe41b1039066595d9e9d9e3ef
TabulateJarl8 <tabulatejarl8@gmail.com>
2021-07-18 12:50:15 -0400

    version bump and fix_encoding improvement

--- tests/fix_encoding.py
+++ tests/fix_encoding.py
@@ -5,22 +5,22 @@ parent = os.path.abspath(os.path.dirname(os.path.dirname(__file__)))
 safe_path = os.path.join(parent, 'safe.txt')
 unsafe_path = os.path.join(parent, 'unsafe.txt')
 
+bad_characters = [("‘", "'"), ("’", "'"), ("“", '"'), ("”", '"')]
+
 with open(safe_path, encoding="utf-8") as f:
 	safe = f.read()
-	
-safe = safe.replace("‘", "'")
-safe = safe.replace("’", "'")
-safe = safe.replace("“", '"')
-safe = safe.replace("”", '"')
+
+for char in bad_characters:
+	safe = safe.replace(char[0], char[1])
+
 with open(safe_path, "w") as f:
 	f.write(safe)
-	
+
 with open(unsafe_path, encoding="utf-8") as f:
 	unsafe = f.read()
-	
-unsafe = unsafe.replace("‘", "'")
-unsafe = unsafe.replace("’", "'")
-unsafe = unsafe.replace("“", '"')
-unsafe = unsafe.replace("”", '"')
+
+for char in bad_characters:
+	unsafe = unsafe.replace(char[0], char[1])
+
 with open(unsafe_path, "w") as f:
 	f.write(unsafe)
