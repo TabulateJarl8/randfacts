06ab415ff86bb8fcbae45a75b9f531992da1059d
TabulateJarl8 <tabulatejarl8@gmail.com>
2022-07-06 23:25:29 -0400

    add more progress, although it may be still slower than rapidfuzz

--- tests/checkduplicates/src/main.rs
+++ tests/checkduplicates/src/main.rs
@@ -1,4 +1,3 @@
-use pyo3::prelude::*;
 use std::{
     process::Command,
     path::PathBuf,
@@ -7,17 +6,25 @@ use std::{
 };
 use itertools::Itertools;
 use indicatif::ProgressBar;
+use fuzzywuzzy::fuzz;
 
 
-fn lines_from_file(filename: &PathBuf) -> Vec<String> {
+fn lines_from_file(filename: &PathBuf, comment: String) -> Vec<(String, String)> {
     let file = File::open(filename).expect("no such file");
     let buf = BufReader::new(file);
     buf.lines()
-        .map(|l| l.expect("Could not parse line"))
+        .map(|l| { (l.expect("Could not parse line"), comment.clone()) })
         .collect()
 }
 
-// #[pyfunction]
+fn partial_match(x_fact: &str, y_fact: &str) -> Option<u8> {
+    let ratio: u8 = fuzz::partial_token_sort_ratio(x_fact, y_fact, true, true);
+    if ratio > 80 {
+        return Some(ratio);
+    }
+    None
+}
+
 fn main() {
     // get project's top level
     let output = Command::new("git")
@@ -34,25 +41,29 @@ fn main() {
     project_root.push("randfacts");
     project_root.push("safe.txt");
 
-    let mut all_facts = lines_from_file(&project_root);
+    let mut all_facts = lines_from_file(&project_root, String::from("safe"));
 
 
     project_root.pop();
     project_root.push("unsafe.txt");
 
-    let mut unsafe_contents = lines_from_file(&project_root);
+    let mut unsafe_contents = lines_from_file(&project_root, String::from("unsafe"));
 
     all_facts.append(&mut unsafe_contents);
 
     // Generate all possible pairs of the facts from safe.txt and unsafe.txt
     // combined
-    let mut matches: Vec<String> = vec![];
+    let mut matches: Vec<u8> = vec![];
     let pb = ProgressBar::new(num_integer::binomial(all_facts.len() as u64, 2));
     let combinations = all_facts.into_iter().combinations(2);
-}
 
-// #[pymodule]
-// fn checkdups(_py: Python, m: &PyModule) -> PyResult<()> {
-//     m.add_function(wrap_pyfunction!(main, m)?)?;
-//     Ok(())
-// }
+    // iterate through all the combinations
+    for (index, facts) in combinations.enumerate() {
+        match partial_match(&facts[0].0, &facts[0].1) {
+            Some(s) => matches.push(s),
+            None => (),
+        }
+        pb.inc(1)
+    }
+
+}
