09f9f22c7857e09c06edfded4f448d56a9daea44
TabulateJarl8 <tabulatejarl8@gmail.com>
2023-11-14 12:16:27 -0500

    add more facts

--- setup.py
+++ setup.py
@@ -31,6 +31,7 @@ setuptools.setup(
 		"Programming Language :: Python :: 3.9",
 		"Programming Language :: Python :: 3.10",
 		"Programming Language :: Python :: 3.11",
+		"Programming Language :: Python :: 3.12",
 		"Natural Language :: English",
 		"License :: OSI Approved :: MIT License",
 		"Operating System :: OS Independent",
